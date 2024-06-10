92c9ca8252e3e5516425019cb43c8149ae68d78d
tkuramot <mabdman512@gmail.com>
2024-06-11 02:44:53 +0900

    implement createElement function

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,34 @@
+function createElement(type, props, ...children) {
+  return {
+    type,
+    props: {
+      ...props,
+      children,
+    },
+  };
+}
+
+function createTextElement(text) {
+  return {
+    type: "TEXT_ELEMENT",
+    props: {
+      nodeValue: text,
+      children: [],
+    },
+  };
+}
+
+const Didact = {
+  createElement,
+};
+
+/** @jsx Didact.createElement **/
 const element = (
   <div id="foo">
     <a>bar</a>
-    <b/>
+    <b />
   </div>
-)
-const container = document.getElementById("root")
-ReactDOM.render(element, container)
+);
+
+const container = document.getElementById("root");
+ReactDOM.render(element, container);
