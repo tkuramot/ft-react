324177d3fe25e750843c8b2c4e181d287db89629
tkuramot <mabdman512@gmail.com>
2024-06-11 22:06:19 +0900

    fibers

--- src/index.js
+++ src/index.js
@@ -20,19 +20,90 @@ function createTextElement(text) {
   };
 }
 
-function render(element, container) {
-  const dom = element.type === "TEXT_ELEMENT"
+function createDom(fiber) {
+  const dom = fiber.type === "TEXT_ELEMENT"
     ? document.createTextNode("")
-    : document.createElement(element.type);
+    : document.createElement(fiber.type);
 
   const isProperty = (key) => key !== "children";
-  Object.keys(element.props).filter(isProperty).forEach((name) => {
-    dom[name] = element.props[name];
+  Object.keys(fiber.props).filter(isProperty).forEach((name) => {
+    dom[name] = fiber.props[name];
   });
 
-  element.props.children.forEach((child) => render(child, dom));
+  return dom;
+}
 
-  container.appendChild(dom);
+function render(element, container) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork = null;
+
+function workLoop(deadline) {
+  let shouldYield = false;
+
+  while (nextUnitOfWork && !shouldYield) {
+    nextUnitOfWork = performUnitOfWork(
+      nextUnitOfWork,
+    );
+
+    shouldYield = deadline.timeRemaining() < 1;
+  }
+  requestIdleCallback(workLoop);
+}
+
+requestIdleCallback(workLoop);
+
+function performUnitOfWork(fiber) {
+  // add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+  let nextFiber = fiber;
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+    nextFiber = nextFiber.parent;
+  }
 }
 
 const Didact = {
